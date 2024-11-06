e8c63418c7d376e4ece363cb96c97a13ed8191de
tjfwltn <tjfwltn1234@gmail.com>
2024-11-07 00:09:27 +0900

    feat : ViewMessage Class 작성
    - InputView에 있는 고정적인 내용 ViewMessage에 상수 처리
    - InputView에서 그걸 가져와 쓰게 수정

---
+++ src/main/java/store/constants/ViewMessage.java
@@ -0,0 +1,10 @@
+package store.constants;
+
+public class ViewMessage {
+
+    public static final String WELCOME = "안녕하세요. W편의점입니다.";
+    public static final String REQUEST_PRODUCT_TO_PURCHASE =
+            "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
+    public static final String ASK_APPLY_MEMBERSHIP = "멤버십 할인을 받으시겠습니까? (Y/N)";
+    public static final String ASK_FOR_ANOTHER_PRODUCT = "감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)";
+}
